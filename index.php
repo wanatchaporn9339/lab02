0cba7948806b0bd25bb80166256a5109e6499f39
wanatchaporn9339 <73693820+wanatchaporn9339@users.noreply.github.com>
2020-11-06 18:13:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Lab</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <style>
+            p{
+                color: slategrey;
+            }
+
+            h5{
+                color: black;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card-deck">
+                <div class="card">
+                  <img src="BANGKOK .jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Bangkok City</h5>
+                    <p class="card-text">Bangkok is the capital and most populous city of Thailand.</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="Seoul.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">South Korea</h5>
+                    <p class="card-text">The name Korea derives from the name Goryeo. The name Goryeo itself was first used by the ancient kingdom of Goguryeo.</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="JAPAN.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Japan</h5>
+                    <p class="card-text">Japan (Japanese: 日本), is an island country in East Asia located in the northwest Pacific Ocean.</p>
+                  </div>
+                  <div class="card-footer">
+                    <img style="border-radius: 50%;" src="img_avatar.png" alt="Avatar" height=25>
+                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="green" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                      </svg>
+                  </div>
+                </div>
+              </div>
+          </div>
+    </body>
+</html>
